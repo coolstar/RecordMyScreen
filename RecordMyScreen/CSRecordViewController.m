f3a55d13ae22ae90a4a7f167692ef7e0bb06e3ed
truehybridx <truehybridx@gmail.com>
2013-01-13 16:02:47 -0600

    More Rotation
    Made both views rotate if on iPad, UI seems to rotate fine

--- RecordMyScreen/CSRecordViewController.m
+++ RecordMyScreen/CSRecordViewController.m
@@ -73,7 +73,11 @@
 }
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    } else {
+        return YES;
+    }
 }
 
 #pragma mark - Starting / Stopping
